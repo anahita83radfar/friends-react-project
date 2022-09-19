e0c4aeaddaab9c2d228351fcc945cbe1be7af11f
anahita83radfar <anahita83radfar@gmail.com>
2022-09-19 20:58:45 +0000

    Adding Search Bar to the Posts page

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -5,22 +5,26 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import styles from "../../styles/PostsPage.module.css";
 import { useLocation } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 
 import NoResults from "../../assets/no-results.png";
 import Asset from "../../components/Asset";
+import Form from "react-bootstrap/Form";
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
   const [loaded, setLoaded] = useState(false);
   const { pathname } = useLocation();
 
+  const [query, setQuery] = useState("");
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
         setPosts(data);
         setLoaded(true);
       } catch (error) {
@@ -29,13 +33,31 @@ function PostsPage({ message, filter = "" }) {
     };
 
     setLoaded(false);
-    fetchPosts();
-  }, [filter, pathname]);
+    const timer = setTimeout(() => {
+      fetchPosts();
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, pathname, query]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search post"
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+          />
+        </Form>
         {loaded ? (
           <>
             {posts.results.length ? (
