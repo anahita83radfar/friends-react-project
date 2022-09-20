7d797d7a467d442b435f77f817da6aca509a3506
anahita83radfar <anahita83radfar@gmail.com>
2022-09-20 09:45:28 +0000

    Add the ‘unfollow’ functionality for profiles

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { axiosReq, axiosRes } from "../api/axiosDefaults";
-import { followHelper } from "../utils/utils";
+import { followHelper, unfollowHelper } from "../utils/utils";
 import { useCurrentUser } from "./CurrentUserContext";
 
 export const ProfileDataContext = createContext();
@@ -42,6 +42,29 @@ export const ProfileDataProvider = ({ children }) => {
     }
   };
 
+  const handleUnfollow = async (clickedProfile) => {
+    try {
+      await axiosRes.delete(`/followers/${clickedProfile.following_id}`);
+
+      setProfileData((prevState) => ({
+        ...prevState,
+        pageProfile: {
+          results: prevState.pageProfile.results.map((profile) =>
+            unfollowHelper(profile, clickedProfile)
+          ),
+        },
+        popularProfiles: {
+          ...prevState.popularProfiles,
+          results: prevState.popularProfiles.results.map((profile) =>
+            unfollowHelper(profile, clickedProfile)
+          ),
+        },
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
     const handleMount = async () => {
       try {
@@ -61,7 +84,9 @@ export const ProfileDataProvider = ({ children }) => {
 
   return (
     <ProfileDataContext.Provider value={profileData}>
-      <SetProfileDataContext.Provider value={{ setProfileData, handleFollow }}>
+      <SetProfileDataContext.Provider
+        value={{ setProfileData, handleFollow, handleUnfollow }}
+      >
         {children}
       </SetProfileDataContext.Provider>
     </ProfileDataContext.Provider>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,43 +1,57 @@
 import { axiosReq } from "../api/axiosDefaults";
 
 // The code taken from the Code Institute moments project
-{/*The reduce method loops through the new page of results from API. 
+{
+  /*The reduce method loops through the new page of results from API. 
 Then new results appends to the existing posts. The some method loops
 through the array of posts in the accumulator. Inside, each accumulator
 item id compares to the current post id from the newly fetched posts array.
 If the some method returned true, this means it found a match and displayed
 that post already. So in this case the accumulator returns without adding
 the post to it. If the some method does not find a match, an array containing
-spread accumulator returns with the new post added to it.*/}
+spread accumulator returns with the new post added to it.*/
+}
 export const fetchMoreData = async (resource, setResource) => {
-    try {
-      const { data } = await axiosReq.get(resource.next);
-      setResource((prevResource) => ({
-        ...prevResource,
-        next: data.next,
-        results: data.results.reduce((acc, cur) => {
-          return acc.some((accResult) => accResult.id === cur.id)
-            ? acc
-            : [...acc, cur];
-        }, prevResource.results),
-      }));
-    } catch (err) {}
-  };
+  try {
+    const { data } = await axiosReq.get(resource.next);
+    setResource((prevResource) => ({
+      ...prevResource,
+      next: data.next,
+      results: data.results.reduce((acc, cur) => {
+        return acc.some((accResult) => accResult.id === cur.id)
+          ? acc
+          : [...acc, cur];
+      }, prevResource.results),
+    }));
+  } catch (err) {}
+};
 
-  export const followHelper = (profile, clickedProfile, following_id) => {
-    return profile.id === clickedProfile.id
-      ? // This is the profile I clicked on,
-        // update its followers count and set its following id
-        {
-          ...profile,
-          followers_count: profile.followers_count + 1,
-          following_id,
-        }
-      : profile.is_owner
-      ? // This is the profile of the logged in user
-        // update its following count
-        { ...profile, following_count: profile.following_count + 1 }
-      : // this is not the profile the user clicked on or the profile
-        // the user owns, so just return it unchanged
-        profile;
+export const followHelper = (profile, clickedProfile, following_id) => {
+  return profile.id === clickedProfile.id
+    ? // This is the profile I clicked on,
+      // update its followers count and set its following id
+      {
+        ...profile,
+        followers_count: profile.followers_count + 1,
+        following_id,
       }
+    : profile.is_owner
+    ? // This is the profile of the logged in user
+      // update its following count
+      { ...profile, following_count: profile.following_count + 1 }
+    : // this is not the profile the user clicked on or the profile
+      // the user owns, so just return it unchanged
+      profile;
+};
+
+export const unfollowHelper = (profile, clickedProfile) => {
+  return profile.id === clickedProfile.id
+    ? {
+        ...profile,
+        followers_count: profile.followers_count - 1,
+        following_id: null,
+      }
+    : profile.is_owner
+    ? { ...profile, following_count: profile.following_count - 1 }
+    : profile;
+};
