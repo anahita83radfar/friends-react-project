9181b63ad173c0d3a8b6d7192bee8627fcd9158c
anahita83radfar <anahita83radfar@gmail.com>
2022-09-20 07:28:55 +0000

    Add dropdown menu to edit and delete comment

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,11 +1,45 @@
 import React from "react";
 import Media from "react-bootstrap/Media";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosRes } from "../../api/axiosDefaults";
 import Avatar from "../../components/Avatar";
 import styles from "../../styles/Comment.module.css";
+import { MoreDropDown } from "../../components/MoreDropdown";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function Comment(props) {
-  const { profile_id, profile_image, owner, updated_at, content } = props;
+    const {
+        profile_id,
+        profile_image,
+        owner,
+        updated_at,
+        content,
+        setPost,
+        setComments,
+        id,
+      } = props;
+    
+      const currentUser = useCurrentUser();
+      const is_owner = currentUser?.username === owner;
+    
+      const handleDelete = async () => {
+        try {
+          await axiosRes.delete(`/comments/${id}`);
+          setPost((prevPost) => ({
+            results: [
+              {
+                ...prevPost.results[0],
+                comments_count: prevPost.results[0].comments_count - 1,
+              },
+            ],
+          }));
+    
+          setComments((prevComments) => ({
+            ...prevComments,
+            results: prevComments.results.filter((comment) => comment.id !== id),
+          }));
+        } catch (error) {}
+      };
 
   return (
     <div>
@@ -19,6 +53,9 @@ function Comment(props) {
           <span className={styles.Date}>{updated_at}</span>
           <p>{content}</p>
         </Media.Body>
+        {is_owner && (
+          <MoreDropDown handleEdit={() => {}} handleDelete={handleDelete} />
+        )}
       </Media>
     </div>
   );
