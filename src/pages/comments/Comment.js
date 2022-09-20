16f869c7216d35631ffa461168e1b3353f5f6b0f
anahita83radfar <anahita83radfar@gmail.com>
2022-09-20 07:44:36 +0000

    Add the edit comment form

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Media from "react-bootstrap/Media";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosRes } from "../../api/axiosDefaults";
@@ -6,58 +6,75 @@ import Avatar from "../../components/Avatar";
 import styles from "../../styles/Comment.module.css";
 import { MoreDropDown } from "../../components/MoreDropdown";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import CommentEditForm from "./CommentEditForm";
 
 function Comment(props) {
-    const {
-        profile_id,
-        profile_image,
-        owner,
-        updated_at,
-        content,
-        setPost,
-        setComments,
-        id,
-      } = props;
-    
-      const currentUser = useCurrentUser();
-      const is_owner = currentUser?.username === owner;
-    
-      const handleDelete = async () => {
-        try {
-          await axiosRes.delete(`/comments/${id}`);
-          setPost((prevPost) => ({
-            results: [
-              {
-                ...prevPost.results[0],
-                comments_count: prevPost.results[0].comments_count - 1,
-              },
-            ],
-          }));
-    
-          setComments((prevComments) => ({
-            ...prevComments,
-            results: prevComments.results.filter((comment) => comment.id !== id),
-          }));
-        } catch (error) {}
-      };
+  const {
+    profile_id,
+    profile_image,
+    owner,
+    updated_at,
+    content,
+    setPost,
+    setComments,
+    id,
+  } = props;
+
+  const [showEditForm, setShowEditForm] = useState(false);
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/comments/${id}`);
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count - 1,
+          },
+        ],
+      }));
+
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.filter((comment) => comment.id !== id),
+      }));
+    } catch (error) {}
+  };
 
   return (
-    <div>
+    <>
       <hr />
       <Media>
         <Link to={`/profiles/${profile_id}`}>
           <Avatar src={profile_image} />
         </Link>
-        <Media.Body>
+        <Media.Body className="align-self-center ml-2">
           <span className={styles.Owner}>{owner}</span>
           <span className={styles.Date}>{updated_at}</span>
-          <p>{content}</p>
+          {showEditForm ? (
+            <CommentEditForm
+              id={id}
+              profile_id={profile_id}
+              content={content}
+              profileImage={profile_image}
+              setComments={setComments}
+              setShowEditForm={setShowEditForm}
+            />
+          ) : (
+            <p>{content}</p>
+          )}
         </Media.Body>
-        {is_owner && (
-          <MoreDropDown handleEdit={() => {}} handleDelete={handleDelete} />
+        {is_owner && !showEditForm && (
+          <MoreDropDown
+            handleEdit={() => setShowEditForm(true)}
+            handleDelete={handleDelete}
+          />
         )}
       </Media>
-    </div>
+    </>
   );
 }
 
---
+++ src/pages/comments/CommentEditForm.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+import Form from "react-bootstrap/Form";
+import { axiosRes } from "../../api/axiosDefaults";
+
+import styles from "../../styles/CommentCreateEditForm.module.css";
+
+function CommentEditForm(props) {
+  const { id, content, setShowEditForm, setComments } = props;
+
+  const [formContent, setFormContent] = useState(content);
+
+  const handleChange = (event) => {
+    setFormContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.put(`/comments/${id}/`, {
+        content: formContent.trim(),
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.map((comment) => {
+          return comment.id === id
+            ? {
+                ...comment,
+                content: formContent.trim(),
+                updated_at: "now",
+              }
+            : comment;
+        }),
+      }));
+      setShowEditForm(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="pr-1">
+        <Form.Control
+          className={styles.Form}
+          as="textarea"
+          value={formContent}
+          onChange={handleChange}
+          rows={1}
+        />
+      </Form.Group>
+      <div className="text-right">
+        <button
+          className={`${styles.Button} ${styles.Text}`}
+          onClick={() => setShowEditForm(false)}
+          type="button"
+        >
+          Cancel
+        </button>
+        <button
+          className={`${styles.Button} ${styles.Text}`}
+          disabled={!content.trim()}
+          type="submit"
+        >
+          Save
+        </button>
+      </div>
+    </Form>
+  );
+}
+
+export default CommentEditForm;
